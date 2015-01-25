1a6596f8b6ffb5dda4a8ff13941b4471e945cc3a
cphyc <admin@cphyc.me>
2015-01-25 17:49:11 +0100

    Clock

--- asm/test.s
+++ asm/test.s
@@ -1,14 +1,27 @@
-ldi R27 00000000
-ldi R26 00111100
+ldi R18 00000000
+ldi R19 00000000
+ldi R20 00000000
 ldi R29 00000000
-ldi R28 00000010
+ldi R28 00000101
 ldi R31 00000000
-ldi R30 00000101
-ld R16 X # Load current timer
-ld R17 Y # Load current tick
-add R17 R16 # Add them
-st X R17 # Update timer
+ldi R30 00000110
+ld R17 Y # tic
+add R18 R17 # add a second if tic
+mov R16 R18 # Test if 60 seconds have elapsed
+subi R16 00111100
+tst R16
+breq 00001101
+addi R19 00000001
+ldi R18 00000000
+mov R16 R19 # Test if 60 minutes have elapsed
+subi R19 00111100
+tst R19
+breq 00000111
+addi R20 00000001
+ldi R19 00000000
+mov R16 R19 # Test if 24 hours have elapsed
+subi R16 00011000
+tst R16
+breq 00000001
 ldi R16 00000000
-st Y R17 # Reset tick
-st Z R17 # Output
-breq 1110111
+breq 11100100 # go back to start (-27)
